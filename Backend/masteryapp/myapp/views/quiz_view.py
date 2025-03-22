fc530c926e8870ff6015434146db2dfd520d03d5
sultanTrash <115166309+sultanTrash@users.noreply.github.com>
2025-03-22 13:22:13 -0400

    Question weighing "algorithm"

--- Backend/masteryapp/myapp/views/quiz_view.py
+++ Backend/masteryapp/myapp/views/quiz_view.py
@@ -12,10 +12,13 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import action
 from django.db import transaction
+        
+from django.utils import timezone
 from django.shortcuts import get_object_or_404
 
 from ..models.course import Course
 from ..models.subject import Subject
+from ..models.question import Question
 from ..models.material_snippet import MaterialSnippet
 from ..serializers.course_serializer import CourseSerializer
 from ..serializers.quiz_serializer import QuizSerializer
@@ -24,6 +27,7 @@ from ..models.material_snippet import MaterialSnippet
 
 from io import BytesIO
 from pdfminer.high_level import extract_text
+import random
 
 class QuizViewSet(viewsets.ModelViewSet):
     queryset = Quiz.objects.all()
@@ -82,6 +86,51 @@ class QuizViewSet(viewsets.ModelViewSet):
         
         # By now we have the source snippets.
         
+        weighted_snippets = []
+        for snippet in source_snippets:
+            subject = snippet.subject
+            mastery = self.compute_mastery_score(self, subject, request.user)
+            weight = self.compute_weight(self, mastery)
+            weighted_snippets.append((snippet, weight))
+
+        selected_snippets = random.choices(
+            population=[snip for snip, wt in weighted_snippets],
+            weights=[wt for snip, wt in weighted_snippets],
+            k=new_quiz.quiz_length
+        )
         
         
+
+        
+
+
+
+
+    def compute_mastery_score(self, subject, user):
+        # Retrieve questions for this user and subject.
+        questions = Question.objects.filter(quiz__user=user, snippet__subject=subject)
+        mastery = 0.0
+        now = timezone.now()
+        
+        for question in questions:
+            if question.is_correct is None:
+                continue # Unanswered question
+            # If question is answered, it's quiz has a completed_at date.
+            days_since = (now - question.quiz.completed_at).days
+            degradation = max(0, 1 - (0.01 * days_since))
+            if question.is_correct:
+                mastery += degradation
+            else:
+                mastery -= degradation
+        return mastery
+    
+    def compute_weight(self, mastery):
+        # but if mastery is negative, we further add the weight: weight = 1 + abs(mastery).
+        # maybe the weighing is to be reworked.
+        if mastery >= 0:
+            return 1 / (1 + mastery)
+        else:
+            return 1 + abs(mastery)
+
+
             

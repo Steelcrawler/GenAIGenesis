50ccdf996e24eba61eaeca66893bd08e08f5c477
sultanTrash <115166309+sultanTrash@users.noreply.github.com>
2025-03-22 01:56:22 -0400

    Models init

--- Backend/masteryapp/myapp/models/course.py
+++ Backend/masteryapp/myapp/models/course.py
@@ -2,7 +2,15 @@ from django.core.validators import RegexValidator
 from django.db import models
 from django.conf import settings
 import uuid
+from django.contrib.auth.models import User
+
 
 class Course(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    icon = models.CharField(max_length=300)
+    image_path = models.CharField(max_length=1000)
+    
     
--- Backend/masteryapp/myapp/models/subcategory.py
+++ Backend/masteryapp/myapp/models/subcategory.py
@@ -4,17 +4,14 @@ from django.conf import settings
 from .course import Course
 import uuid
 
-class ClassMaterial(models.Model):
+class Subcategory(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    file_name = models.CharField(max_length=500)
+    name = models.CharField(max_length=200)
     course = models.ForeignKey(
         Course,
         on_delete=models.CASCADE,
         related_name="material"
     )
-    weight = models.PositiveSmallIntegerField(
-        choices=[(i, str(i)) for i in range(20)],
-        null=True
-    )
+    
     
     

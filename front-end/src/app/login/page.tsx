2779afeb980e6b89ab23627c841007fa2fd6f003
Hrithik Shah <hrithik21.shah@gmail.com>
2025-03-22 01:48:28 -0400

    Fix styling bug for frontend

--- front-end/src/app/login/page.tsx
+++ front-end/src/app/login/page.tsx
@@ -21,7 +21,7 @@ export default function LoginPage() {
     }
 
     if (email === "test@example.com" && password === "123") {
-      router.push("/temp");
+      router.push("/courses");
     } else {
       setError('Incorrect email or password');
     }

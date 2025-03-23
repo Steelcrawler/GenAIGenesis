387e840d3c57af61984361349bcda783eb3c6e62
Hrithik Shah <hrithik21.shah@gmail.com>
2025-03-23 07:49:46 -0400

    Fix bug

--- front-end/src/components/SyncedProvider.tsx
+++ front-end/src/components/SyncedProvider.tsx
@@ -7,43 +7,25 @@ import { QuizProvider } from "@/context/QuizViewContext";
 import { CurrentCourseProvider } from "@/context/CurrentCourseContext";
 import { QuestionProvider } from "@/context/QuestionContext";
 import { SubjectProvider } from "@/context/SubjectContext";
-<<<<<<< HEAD
-import { ResponseProvider } from "@/context/ResponseContext"
+import { ResponseProvider } from "@/context/ResponseContext";
+import { MaterialSnippetProvider } from "@/context/MaterialSnippetContext"
 
 export const SyncedProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <ResponseProvider>
-    <QuestionProvider>
-      <CurrentCourseProvider>
-        <QuizProvider>
-          <MaterialProvider>
-            <CourseProvider>
-              <SubjectProvider>
-                {children}
-              </SubjectProvider>
-            </CourseProvider>
-          </MaterialProvider>
-        </QuizProvider>
-      </CurrentCourseProvider>
-    </QuestionProvider>
-  </ResponseProvider>
-=======
-import {MaterialSnippetProvider} from "@/context/MaterialSnippetContext";
-
-export const SyncedProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <QuestionProvider>
-    <CurrentCourseProvider>
-      <QuizProvider>
-        <MaterialProvider>
-          <CourseProvider>
-            <SubjectProvider>
-              <MaterialSnippetProvider>
-                {children}
-              </MaterialSnippetProvider>
-            </SubjectProvider>
-          </CourseProvider>
-        </MaterialProvider>
-      </QuizProvider>
-    </CurrentCourseProvider>
-  </QuestionProvider>
->>>>>>> 86d97c45ea815b7bf51b62b423299e9d1bf1af16
+  <MaterialSnippetProvider>
+    <ResponseProvider>
+      <QuestionProvider>
+        <CurrentCourseProvider>
+          <QuizProvider>
+            <MaterialProvider>
+              <CourseProvider>
+                <SubjectProvider>
+                  {children}
+                </SubjectProvider>
+              </CourseProvider>
+            </MaterialProvider>
+          </QuizProvider>
+        </CurrentCourseProvider>
+      </QuestionProvider>
+    </ResponseProvider>
+  </MaterialSnippetProvider>
 );
